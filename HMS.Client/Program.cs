989b8cbf210550a164d9aec1586d2dfc51577e50
Nishit Patel <pnishit238@gmail.com>
2025-10-30 18:04:06 +0530

    Basic setup for HMS.Client to use keycloak.

--- HMS.Client/Program.cs
+++ HMS.Client/Program.cs
@@ -2,12 +2,19 @@ using HMS.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Configures the app to read settings from "Oidc" in appsettings.json
+    builder.Configuration.Bind("Oidc", options.ProviderOptions);
+});
+
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
